7b9aaf0de1dc49f315d48bb59158afefeb853beb
Brian Mugo <brayomugo2015@gmail.com>
2021-07-17 11:42:16 +0300

    - Github Client - Github Repo - Github Service

---
+++ app/src/main/java/com/project/rxjavaproject/GithubClient.java
@@ -0,0 +1,43 @@
+package com.project.rxjavaproject;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+
+public class GithubClient {
+
+    private static final String GITHUB_BASE_URL = "https://api.github.com/";
+
+    private static GithubClient instance;
+    private GithubService githubService;
+
+    private GithubClient () {
+        final Gson gson =
+                new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+        final Retrofit retrofit = new Retrofit.Builder().baseUrl(GITHUB_BASE_URL)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        githubService = retrofit.create(GithubService.class);
+    }
+
+    public static GithubClient getInstance() {
+        if (instance == null) {
+            instance = new GithubClient();
+        }
+        return instance;
+    }
+
+    public Observable<List<GithubRepo>> getStarredRepos(@NonNull String userName) {
+        return githubService.getStarredRepositories(userName);
+    }
+}
---
+++ app/src/main/java/com/project/rxjavaproject/GithubService.java
@@ -0,0 +1,14 @@
+package com.project.rxjavaproject;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface GithubService {
+
+    @GET("users/{user}/starred")
+    Observable<List<GithubRepo>>
+    getStarredRepositories(@Path("user") String userName);
+}
